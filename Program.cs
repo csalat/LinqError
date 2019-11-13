760cbe15a4110631d80581fee9c25d37ce58b90d
Csaba Salat <csalat@globalblue.com>
2019-11-13 16:12:42 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = TestClass.Test();
+        }
+    }
+
+
+    public static class TestClass
+    {
+        public static string Test()
+        {
+            IEnumerable<string> list = new List<string>() {"one"};
+            var preData = GetData();
+            Dictionary<string, object> data = preData.Pick(list);
+            return data.Any() ? GoodCase(data) : null;
+        }
+
+        private static string GoodCase(Dictionary<string, object> identity)
+        {
+            return "good";
+        }
+
+        private static Dictionary<string, object> GetData()
+        {
+            ;
+          return new Dictionary<string, object> {{"one", "two"}};
+        }
+    }
+
+    public static class Ext
+    {
+        public static Dictionary<string, object> Pick(this Dictionary<string, object> source, IEnumerable<string> keys)
+        {
+            IEnumerable<KeyValuePair<string, object>> query =
+                from f in source
+                where keys.Any(name => name == f.Key)
+                select f;
+            return query.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+    }
+
+}
